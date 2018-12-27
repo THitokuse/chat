a720ff174bf558721e4eb72081a85944f500fd34
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-12-27 12:33:20 +0900

    チャットルームのモデル化
    joinとleaveのチャネル
    マップclients
    の追加

--- room.go
+++ room.go
@@ -3,4 +3,13 @@ package main
 type room struct {
   //forwardは他のクライアントに転送するためのメッセージを保持するチャンネルです。
   forward chan []byte
+
+  //joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+  join chan *client
+
+  //leaveはチャットルームから退室しようとしているクライアントのためのチャネルです。
+  leave chan *client
+
+  //clientsには在室している全てのクライアントが保持されます。
+  clients map[*client]bool
 }
