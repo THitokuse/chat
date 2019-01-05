f963dd2e9157a73eeda3a9316effc194feb8f7e8
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-01-05 20:51:51 +0900

    テスト成功　カバレッジ100%達成

--- trace/tracer.go
+++ trace/tracer.go
@@ -2,6 +2,7 @@ package trace
 
 import (
   "io"
+  "fmt"
 )
 //Tracerはコード内での出来事を記録できるオブジェクトを表すインターフェースです。
 type Tracer interface {
@@ -10,7 +11,10 @@ type Tracer interface {
 type tracer struct {
   out io.Writer
 }
-func (t *tracer) Trace(a...interface{}) {}
+func (t *tracer) Trace(a...interface{}) {
+  t.out.Write([]byte(fmt.Sprint(a...)))
+  t.out.Write([]byte("\n"))
+}
 
 func New(w io.Writer) Tracer {
   return &tracer{out: w}
