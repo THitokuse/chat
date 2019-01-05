a88769c333cebd159a02553133be0657aac07dca
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-01-05 20:49:01 +0900

    red-green testing　「''という誤った文字列が出力されました」と表示

--- trace/tracer.go
+++ trace/tracer.go
@@ -7,7 +7,11 @@ import (
 type Tracer interface {
   Trace(...interface{})
 }
+type tracer struct {
+  out io.Writer
+}
+func (t *tracer) Trace(a...interface{}) {}
 
 func New(w io.Writer) Tracer {
-  return nil
+  return &tracer{out: w}
 }
