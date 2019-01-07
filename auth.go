48457b5318e9a4e42730a9cfa8dcc1bcae82ffe4
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2019-01-07 17:44:50 +0900

    auth.goにloginHandlerを定義
    loginHandlerはサードパーティへのログイン処理を受け持つ。
    strings.Splitはパスの文字列を分割するメソッド

--- auth.go
+++ auth.go
@@ -1,10 +1,14 @@
 package main
 import (
   "net/http"
+  "strings"
+  "log"
+  "fmt"
 )
 type authHandler struct {
   next http.Handler
 }
+
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
     //未承認
@@ -20,4 +24,18 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 func MustAuth(handler http.Handler) http.Handler {
   return &authHandler{next: handler}
+}
+//loginHandlerはサードパーティへのログインの処理を受け持ちます。
+//パスの形式：/auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+  segs := strings.Split(r.URL.Path, "/")
+  action := segs[2]
+  provider := segs[3]
+  switch action {
+  case "login":
+    log.Println("TODO: ログイン処理", provider)
+  default:
+    w.WriteHeader(http.StatusNotFound)
+    fmt.Fprintf(w, "アクション%sには非対応です", action)
+  }
 }
